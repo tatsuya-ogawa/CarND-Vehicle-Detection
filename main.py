9b4d2afb79403066d7edf621e68e55d019dc1672
Tatsuya_Ogawa <Tatsuya_Ogawa@jmas.co.jp>
2017-09-16 14:42:32 +0900

    first commit.

--- main.py
+++ main.py
@@ -76,73 +76,60 @@ def find_cars(img,
     return box_list
 
 
-def process_images():
-    # cap = cv2.VideoCapture(video_name)
-    # fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    # out = cv2.VideoWriter('output.avi', fourcc, 20, (1280, 720))
-    # ystart = 400
-    # ystop = 656
-    tmp_dic = {
-        "heatmap_list": [],
-        "dist_pickle": pickle.load(open("svc_pickle.p", "rb"))
-    }
+tmp_dic = {
+    "heatmap_list": [],
+    "dist_pickle": pickle.load(open("svc_pickle.p", "rb"))
+}
 
-    def process_image(src_image):
-        scales = [1.5, 2.0]
-        y_starts = [400, 400]
-        y_stops = [556, 656]
-        dist_pickle = tmp_dic["dist_pickle"]
 
-        svc = dist_pickle["svc"]
-        X_scaler = dist_pickle["scaler"]
-        orient = dist_pickle["orient"]
-        pix_per_cell = dist_pickle["pix_per_cell"]
-        cell_per_block = dist_pickle["cell_per_block"]
-        spatial_size = dist_pickle["spatial_size"]
-        hist_bins = dist_pickle["hist_bins"]
-        heatmap_weight = np.array([0.1, 0.2, 0.3, 0.4], dtype=np.float)
+def process_image(src_image):
+    scales = [1.5, 2.0]
+    y_starts = [400, 400]
+    y_stops = [556, 656]
+    dist_pickle = tmp_dic["dist_pickle"]
 
-        heatmap_list = tmp_dic["heatmap_list"]
-        box_list = []
-        for scale, ystart, ystop in zip(scales, y_starts, y_stops):
-            box_list_tmp = find_cars(src_image, ystart, ystop, scale, svc, X_scaler, orient, pix_per_cell,
-                                     cell_per_block,
-                                     spatial_size,
-                                     hist_bins)
-            box_list.extend(box_list_tmp)
+    svc = dist_pickle["svc"]
+    X_scaler = dist_pickle["scaler"]
+    orient = dist_pickle["orient"]
+    pix_per_cell = dist_pickle["pix_per_cell"]
+    cell_per_block = dist_pickle["cell_per_block"]
+    spatial_size = dist_pickle["spatial_size"]
+    hist_bins = dist_pickle["hist_bins"]
+    heatmap_weight = np.array([0.1, 0.2, 0.3, 0.4], dtype=np.float)
 
-        heat = np.zeros_like(src_image[:, :, 0]).astype(np.float)
-        heat = add_heat(heat, box_list)
-        heat = apply_threshold(heat, 1)
-        current_heatmap = np.clip(heat, 0, 255)
+    heatmap_list = tmp_dic["heatmap_list"]
+    box_list = []
+    for scale, ystart, ystop in zip(scales, y_starts, y_stops):
+        box_list_tmp = find_cars(src_image, ystart, ystop, scale, svc, X_scaler, orient, pix_per_cell,
+                                 cell_per_block,
+                                 spatial_size,
+                                 hist_bins)
+        box_list.extend(box_list_tmp)
 
-        heatmap_list.append(current_heatmap)
-        heatmap_list = heatmap_list[-min(len(heatmap_weight), len(heatmap_list)):]
-        heatmap = np.sum(
-            np.array(heatmap_list, dtype=np.float) * heatmap_weight[-len(heatmap_list):].reshape((-1, 1, 1)),
-            axis=0)
-        heatmap = apply_threshold(heatmap, sum(heatmap_weight[-len(heatmap_list):]) * 2)
+    heat = np.zeros_like(src_image[:, :, 0]).astype(np.float)
+    heat = add_heat(heat, box_list)
+    heat = apply_threshold(heat, 1)
+    current_heatmap = np.clip(heat, 0, 255)
 
-        labels = label(heatmap)
-        draw_img = draw_labeled_bboxes(np.copy(src_image), labels)
-        # for box in box_list:
-        #     cv2.rectangle(draw_img, box[0],
-        #                   box[1], (255, 0, 0), 6)
-        # cv2.rectangle(draw_img, (0, 400),
-        #               (1280, 656), (0, 255, 0), 6)
-        # cv2.rectangle(draw_img, (0, 400),
-        #               (1280, 464), (0, 255, 0), 6)
+    heatmap_list.append(current_heatmap)
+    heatmap_list = heatmap_list[-min(len(heatmap_weight), len(heatmap_list)):]
+    heatmap = np.sum(
+        np.array(heatmap_list, dtype=np.float) * heatmap_weight[-len(heatmap_list):].reshape((-1, 1, 1)),
+        axis=0)
+    heatmap = apply_threshold(heatmap, sum(heatmap_weight[-len(heatmap_list):]) * 2)
 
-        # out.write(draw_img)
-        tmp_dic["heatmap_list"] = heatmap_list
-        return draw_img
+    labels = label(heatmap)
+    draw_img = draw_labeled_bboxes(np.copy(src_image), labels)
+
+    tmp_dic["heatmap_list"] = heatmap_list
+    return draw_img
 
 
 def main():
     video_name = 'project_video.mp4'
     clip = VideoFileClip(video_name)
 
-    clip = clip.fl_image(process_images)
+    clip = clip.fl_image(process_image)
     video_output = 'project_out.mp4'
     clip.write_videofile(video_output, audio=False)
 
